b2a1af30259b9203641e4507d740eabcfd4db1c7
Ahmed <faresaselem@gmail.com>
2024-08-14 22:18:51 +0000

    done

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,21 +1,14 @@
-# 0-strace_is_your_friend.pp
-# Puppet manifest to fix Apache configuration issue causing 500 Internal Server Error
+# Increse the request's limit
 
-file { '/var/www/html/index.html':
-  ensure  => file,
-  content => '<html><body><h1>It works!</h1></body></html>',
-  owner   => 'www-data',
-  group   => 'www-data',
-  mode    => '0644',
+# Increse ulimit value
+exec { 'fix-config-nginx':
+  onlyif  => 'test -e /etc/default/nginx',
+  command => 'sed -i "5s/[0-9]\+/$( ulimit -n )/" /etc/default/nginx',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
 }
 
-service { 'apache2':
-  ensure => running,
-  enable => true,
-}
-
-file { '/etc/apache2/sites-available/000-default.conf':
-  ensure  => file,
-  source  => 'puppet:///modules/your_module/000-default.conf',
-  notify  => Service['apache2'],
+# Restart nginex service
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
 }
